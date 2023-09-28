b0c82266a73ddbca10acb398d7925ed609eb5725
CartUniverse <sebastianeshelley@gmail.com>
2023-09-28 16:00:06 -0700

    Add option to keep original images

--- scripts/CFGRescale.py
+++ scripts/CFGRescale.py
@@ -13,7 +13,6 @@ from PIL import Image
 from ldm.modules.diffusionmodules.util import make_ddim_sampling_parameters, noise_like
 
 
-
 class Script(scripts.Script):
 
     def __init__(self):
@@ -50,15 +49,18 @@ class Script(scripts.Script):
             rescale = gr.Slider(label="CFG Rescale", show_label=False, minimum=0.0, maximum=1.0, step=0.01, value=0.0)
             with gr.Row():
                 recolor = gr.Checkbox(label="Auto Color Fix", default=False)
-                rec_strength = gr.Slider(label="Fix Strength", interactive=True, visible=False, elem_id=self.elem_id("rec_strength"), minimum=0.1, maximum=10.0, step=0.1, value=1.0)
+                rec_strength = gr.Slider(label="Fix Strength", interactive=True, visible=False,
+                                         elem_id=self.elem_id("rec_strength"), minimum=0.1, maximum=10.0, step=0.1,
+                                         value=1.0)
+                show_original = gr.Checkbox(label="Keep Original Images", elem_id=self.elem_id("show_original"), visible=False, default=False)
 
             def show_recolor_strength(rec_checked):
-                return gr.update(visible=rec_checked)
+                return [gr.update(visible=rec_checked), gr.update(visible=rec_checked)]
 
             recolor.change(
                 fn=show_recolor_strength,
                 inputs=recolor,
-                outputs=rec_strength
+                outputs=[rec_strength, show_original]
             )
 
         self.infotext_fields = [
@@ -68,7 +70,7 @@ class Script(scripts.Script):
         self.paste_field_names = []
         for _, field_name in self.infotext_fields:
             self.paste_field_names.append(field_name)
-        return [rescale, recolor, rec_strength]
+        return [rescale, recolor, rec_strength, show_original]
 
     def cfg_replace(self, x_out, conds_list, uncond, cond_scale):
         denoised_uncond = x_out[-uncond.shape[0]:]
@@ -87,7 +89,7 @@ class Script(scripts.Script):
 
         return denoised
 
-    def process(self, p, rescale, recolor, rec_strength):
+    def process(self, p, rescale, recolor, rec_strength, show_original):
 
         if globals()['enable_furry_cocks']:
             globals()['cfg_rescale_fi'] = rescale
@@ -100,15 +102,15 @@ class Script(scripts.Script):
         if recolor:
             p.extra_generation_params["Auto Color Fix Strength"] = rec_strength
 
-    def postprocess(self, p, processed, rescale, recolor, rec_strength):
+    def postprocess(self, p, processed, rescale, recolor, rec_strength, show_original):
         sd_samplers_kdiffusion.CFGDenoiser.combine_denoised = self.old_denoising
 
         def postfix(img, rec_strength):
             prec = 0.0005 * rec_strength
             r, g, b = img.split()
-            #r_min, r_max = np.percentile(r, p), np.percentile(r, 100.0 - p)
-            #g_min, g_max = np.percentile(g, p), np.percentile(g, 100.0 - p)
-            #b_min, b_max = np.percentile(b, p), np.percentile(b, 100.0 - p)
+            # r_min, r_max = np.percentile(r, p), np.percentile(r, 100.0 - p)
+            # g_min, g_max = np.percentile(g, p), np.percentile(g, 100.0 - p)
+            # b_min, b_max = np.percentile(b, p), np.percentile(b, 100.0 - p)
             rh, rbins = np.histogram(r, 256, (0, 256))
             tmp = np.where(rh > rh.sum() * prec)[0]
             r_min = tmp.min()
@@ -129,9 +131,9 @@ class Script(scripts.Script):
                 for j in range(img.height):
                     pix = img.getpixel((i, j))
                     tmp = [0] * 3
-                    #tmp[0] = int((np.clip(pix[0], r_min, r_max) - r_min) / (r_max - r_min) * 255)
-                    #tmp[1] = int((np.clip(pix[1], g_min, g_max) - g_min) / (g_max - g_min) * 255)
-                    #tmp[2] = int((np.clip(pix[2], b_min, b_max) - b_min) / (b_max - b_min) * 255)
+                    # tmp[0] = int((np.clip(pix[0], r_min, r_max) - r_min) / (r_max - r_min) * 255)
+                    # tmp[1] = int((np.clip(pix[1], g_min, g_max) - g_min) / (g_max - g_min) * 255)
+                    # tmp[2] = int((np.clip(pix[2], b_min, b_max) - b_min) / (b_max - b_min) * 255)
                     tmp[0] = int(255 * (min(max(pix[0], r_min), r_max) - r_min) / (r_max - r_min))
                     tmp[1] = int(255 * (min(max(pix[1], g_min), g_max) - g_min) / (g_max - g_min))
                     tmp[2] = int(255 * (min(max(pix[2], b_min), b_max) - b_min) / (b_max - b_min))
@@ -141,24 +143,41 @@ class Script(scripts.Script):
 
         if recolor:
             n_img = len(processed.images)
-            for i in range(n_img):
-                res_img = postfix(processed.images[i], rec_strength)
-                if n_img > 1 and i != 0:
-                    processed.images.extend([res_img])
-                elif n_img == 1 or not opts.return_grid:
-                    processed.images.extend([res_img])
-                
-                # Save images to disk
-                if opts.samples_save:
-                    ind = i
-                    if (i > 0 and n_img > 1) and opts.grid_save:
-                        ind = i-1
-
-                    prompt_infotext = processing.create_infotext(p, p.all_prompts, p.all_seeds, p.all_subseeds, index=ind)
+            if not show_original:
+                for i in range(n_img):
+                    doit = False
                     if n_img > 1 and i != 0:
-                        images.save_image(res_img, p.outpath_samples, "", seed=p.all_seeds[i-1], prompt=p.all_prompts[i-1], info=prompt_infotext, p=p, suffix="colorfix")
+                        doit = True
                     elif n_img == 1 or not opts.return_grid:
-                        images.save_image(res_img, p.outpath_samples, "", seed=p.all_seeds[i], prompt=p.all_prompts[i], info=prompt_infotext, p=p, suffix="colorfix")
+                        doit = True
+
+                    if doit:
+                        res_img = postfix(processed.images[i], rec_strength)
+                        processed.images[i] = res_img
+                        if opts.samples_save:
+                            images.save_image(res_img, p.outpath_samples, "", seed=p.all_seeds[i], prompt=p.all_prompts[i],
+                                              info=prompt_infotext, p=p, suffix="colorfix")
+            else:
+                for i in range(n_img):
+                    res_img = postfix(processed.images[i], rec_strength)
+                    if n_img > 1 and i != 0:
+                        processed.images.extend([res_img])
+                    elif n_img == 1 or not opts.return_grid:
+                        processed.images.extend([res_img])
+
+                    ind = i
+                    if (i != 0 and n_img > 1) and opts.return_grid:
+                        ind = i - 1
+                    prompt_infotext = processing.create_infotext(p, p.all_prompts, p.all_seeds, p.all_subseeds, index=ind)
+                    # Save images to disk
+                    if opts.samples_save:
+                        if n_img > 1 and i != 0:
+                            images.save_image(res_img, p.outpath_samples, "", seed=p.all_seeds[i - 1],
+                                              prompt=p.all_prompts[i - 1], info=prompt_infotext, p=p, suffix="colorfix")
+                        elif n_img == 1 or not opts.return_grid:
+                            images.save_image(res_img, p.outpath_samples, "", seed=p.all_seeds[i], prompt=p.all_prompts[i],
+                                              info=prompt_infotext, p=p, suffix="colorfix")
+
 
 def on_infotext_pasted(infotext, params):
     if "CFG Rescale" not in params:
@@ -174,5 +193,6 @@ def on_infotext_pasted(infotext, params):
 
     if "DDIM Trailing" not in params:
         params["DDIM Trailing"] = False
+
 
 script_callbacks.on_infotext_pasted(on_infotext_pasted)
